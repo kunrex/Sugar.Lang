c0d91d819de0ac29361acdcf403cb1765901bcdf
kunrex <74914319+kunrex@users.noreply.github.com>
2025-02-19 15:32:46 +0530

    updated code

--- src/Analysis/Structure/Core/describable.h
+++ src/Analysis/Structure/Core/describable.h
@@ -13,8 +13,9 @@ namespace Analysis::Structure::Core
             explicit Describable(Enums::Describer describer);
 
         public:
-            [[nodiscard]] Enums::Describer Describer() const;
+            [[nodiscard]] bool ValidateDescriber(Enums::Describer allowed) const;
 
+            [[nodiscard]] bool MatchDescriber(Enums::Describer expected) const;
             [[nodiscard]] bool CheckDescriber(Enums::Describer describer) const;
             [[nodiscard]] bool ValidateDescriber(Enums::Describer allowed) const;
     };
