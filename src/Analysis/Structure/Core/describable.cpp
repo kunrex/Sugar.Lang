5c61e0ccaee02bf2c9c1ad4e9f2bb7be19a0886e
kunrex <74914319+kunrex@users.noreply.github.com>
2025-02-19 15:33:35 +0530

    updated code

--- src/Analysis/Structure/Core/describable.cpp
+++ src/Analysis/Structure/Core/describable.cpp
@@ -7,7 +7,7 @@ namespace Analysis::Structure::Core
     Describable::Describable(const Describer describer) : describer(describer)
     { }
 
-    Enums::Describer Describable::Describer() const { return describer; }
+    bool Describable::ValidateDescriber(const Describer allowed) const { return (allowed & describer) == describer; }
 
     bool Describable::CheckDescriber(const Enums::Describer describer) const { return (describer & this->describer) == describer; }
     bool Describable::ValidateDescriber(const Enums::Describer allowed) const { return (allowed & describer) == describer; }
