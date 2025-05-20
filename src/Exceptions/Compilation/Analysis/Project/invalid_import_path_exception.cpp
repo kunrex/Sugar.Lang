8deeecd59a4a29ebc19789e8902b900de5d22ca2
kunal <kunal22102005@gmail.com>
2025-05-20 13:57:33 +0530

    Updated error message

--- src/Exceptions/Compilation/Analysis/Project/invalid_import_path_exception.cpp
+++ src/Exceptions/Compilation/Analysis/Project/invalid_import_path_exception.cpp
@@ -10,6 +10,6 @@ using namespace Analysis::Structure;
 
 namespace Exceptions
 {
-    InvalidImportPathException::InvalidImportPathException(const string& path, const unsigned long index, const SourceFile* const source) : LogException(std::format("Path at `{}` was not found. Import statements use a local path.", path), index, source)
+    InvalidImportPathException::InvalidImportPathException(const string& path, const unsigned long index, const SourceFile* const source) : LogException(std::format("Path at `{}` was not found. Import statements use a local path", path), index, source)
     { }
 }
