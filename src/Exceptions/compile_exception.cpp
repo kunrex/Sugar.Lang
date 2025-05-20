50be5e6433c9e393e7e77df5c8426f41df313274
kunal <kunal22102005@gmail.com>
2025-05-20 13:59:01 +0530

    updated function implementation

--- src/Exceptions/compile_exception.cpp
+++ src/Exceptions/compile_exception.cpp
@@ -5,6 +5,6 @@ namespace Exceptions
     CompileException::CompileException(std::string message) : message(std::move(message))
     { }
 
-    const char* CompileException::what() const noexcept { return c_str; }
+    const char* CompileException::what() const noexcept { return message.c_str(); }
 }
 
